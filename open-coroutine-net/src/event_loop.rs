bb0cb3f601937e6bc7dac7a5937a5a5e6910617a
hailang <a1936978077@hotmail.com>
2023-09-23 10:42:57 +0800

    try pass CI

--- open-coroutine-net/src/event_loop.rs
+++ open-coroutine-net/src/event_loop.rs
@@ -354,7 +354,7 @@ impl<'e> EventLoop<'e> for EventLoopImpl<'e> {
     }
 }
 
-#[cfg(test)]
+#[cfg(all(test, not(debug_assertions)))]
 mod tests {
     use super::*;
 
@@ -380,6 +380,44 @@ mod tests {
     #[test]
     fn test_wait() -> std::io::Result<()> {
         let event_loop = EventLoopImpl::default();
+        event_loop.set_max_size(1);
+        let task_name = uuid::Uuid::new_v4().to_string();
+        _ = event_loop.submit(None, |_| panic!("test panic, just ignore it"), None);
+        let result = event_loop.submit_and_wait(
+            Some(task_name.clone()),
+            |_| {
+                println!("2");
+                Some(2)
+            },
+            None,
+            Duration::from_millis(100),
+        );
+        assert_eq!(Some((task_name, Ok(Some(2)))), result.unwrap());
+        event_loop.stop(Duration::from_secs(1))
+    }
+
+    #[test]
+    fn test_simple_auto() -> std::io::Result<()> {
+        let event_loop = EventLoopImpl::default().start()?;
+        event_loop.set_max_size(1);
+        _ = event_loop.submit(None, |_| panic!("test panic, just ignore it"), None);
+        _ = event_loop.submit(
+            None,
+            |_| {
+                println!("2");
+                Some(2)
+            },
+            None,
+        );
+        let now = open_coroutine_timer::now();
+        event_loop.slice_wait(Some(Duration::from_millis(10)), true)?;
+        assert!(open_coroutine_timer::now() - now >= 10_000_000);
+        event_loop.stop(Duration::from_secs(1))
+    }
+
+    #[test]
+    fn test_wait_auto() -> std::io::Result<()> {
+        let event_loop = EventLoopImpl::default().start()?;
         event_loop.set_max_size(1);
         let task_name = uuid::Uuid::new_v4().to_string();
         _ = event_loop.submit(None, |_| panic!("test panic, just ignore it"), None);
