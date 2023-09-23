88772f4d3cf853347795087dc3173000e8eb4853
hailang <a1936978077@hotmail.com>
2023-09-23 15:57:09 +0800

    try pass CI

--- open-coroutine-net/src/event_loop.rs
+++ open-coroutine-net/src/event_loop.rs
@@ -358,7 +358,25 @@ impl<'e> EventLoop<'e> for EventLoopImpl<'e> {
 mod tests {
     use super::*;
 
-    #[cfg(not(debug_assertions))]
+    #[test]
+    fn test_simple() -> std::io::Result<()> {
+        let event_loop = EventLoopImpl::default();
+        event_loop.set_max_size(1);
+        _ = event_loop.submit(None, |_| panic!("test panic, just ignore it"), None);
+        _ = event_loop.submit(
+            None,
+            |_| {
+                println!("2");
+                Some(2)
+            },
+            None,
+        );
+        let now = open_coroutine_timer::now();
+        event_loop.slice_wait(Some(Duration::from_millis(10)), true)?;
+        assert!(open_coroutine_timer::now() - now >= 10_000_000);
+        event_loop.stop(Duration::from_secs(30))
+    }
+
     #[test]
     fn test_wait() -> std::io::Result<()> {
         let event_loop = EventLoopImpl::default();
@@ -378,24 +396,24 @@ mod tests {
         event_loop.stop(Duration::from_secs(3))
     }
 
-    // #[test]
-    // fn test_simple_auto() -> std::io::Result<()> {
-    //     let event_loop = EventLoopImpl::default().start()?;
-    //     event_loop.set_max_size(1);
-    //     _ = event_loop.submit(None, |_| panic!("test panic, just ignore it"), None);
-    //     _ = event_loop.submit(
-    //         None,
-    //         |_| {
-    //             println!("2");
-    //             Some(2)
-    //         },
-    //         None,
-    //     );
-    //     let now = open_coroutine_timer::now();
-    //     event_loop.slice_wait(Some(Duration::from_millis(10)), true)?;
-    //     assert!(open_coroutine_timer::now() - now >= 10_000_000);
-    //     event_loop.stop(Duration::from_secs(30))
-    // }
+    #[test]
+    fn test_simple_auto() -> std::io::Result<()> {
+        let event_loop = EventLoopImpl::default().start()?;
+        event_loop.set_max_size(1);
+        _ = event_loop.submit(None, |_| panic!("test panic, just ignore it"), None);
+        _ = event_loop.submit(
+            None,
+            |_| {
+                println!("2");
+                Some(2)
+            },
+            None,
+        );
+        let now = open_coroutine_timer::now();
+        event_loop.slice_wait(Some(Duration::from_millis(10)), true)?;
+        assert!(open_coroutine_timer::now() - now >= 10_000_000);
+        event_loop.stop(Duration::from_secs(30))
+    }
 
     #[test]
     fn test_wait_auto() -> std::io::Result<()> {
